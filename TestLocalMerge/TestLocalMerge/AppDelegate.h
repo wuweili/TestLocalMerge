dc227c9572ecc2352a99ec86cb4173452bbfe549
张蒙蒙 <zmm810827@163.com>
2017-07-19 09:44:43 +0800

    no message

---
+++ TestLocalMerge/TestLocalMerge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestLocalMerge
+//
+//  Created by 张蒙蒙 on 2017/7/19.
+//  Copyright © 2017年 张蒙蒙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestLocalMerge/TestLocalMerge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestLocalMerge
+//
+//  Created by 张蒙蒙 on 2017/7/19.
+//  Copyright © 2017年 张蒙蒙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
